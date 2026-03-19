7c4b07ee1dc14f9df2b023ec5d0ce5d777d9415b
AdamChopper835 <adamchopper83@gmail.com>
2026-03-19 09:37:17 +0100

    index.vue

--- app/index.vue
+++ app/index.vue
@@ -162,7 +162,7 @@
                 </svg>
               </div>
               <div class="action-title">
-                Planifier / Supprimer
+                Planifier
                 <svg class="action-arrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                   <line x1="5" x2="19" y1="12" y2="12"></line>
                   <polyline points="12 5 19 12 12 19"></polyline>
